449111db629fac8a520bbfec0c48f6f5fb62e29e
Leonnardo Nascimento <leonnardoenriquesn@gmail.com>
2022-02-17 20:50:01 -0800

    second

--- js/main.js
+++ js/main.js
@@ -1,20 +1,64 @@
 document.addEventListener("DOMContentLoaded", () =>{
     createSquares();
 
-    let guessedWords  = [[]]
-    let availableSpace = 1
+    let guessedWords  = [[]];
+    let availableSpace = 1;
 
-    let word = 'dairy'
+    let word = 'dairy';
+
+    let guessedWordCount = 0;
+
 
     const keys = document.querySelectorAll(".keyboard-row button");
+
+
+    function getTileColor(letter, index){
+        const isCorrectLetter = word.includes(letter)
+
+        if(!isCorrectLetter){
+            return "rgb(58, 58, 60)"
+        }
+
+        const letterInThatPosition =word.charAt(index)
+        const isCorrectPosition = letter === letterInThatPosition
+
+        if (isCorrectPosition){
+            return "rgb(83, 141, 78)"
+        }
+
+        return "rgb(181, 159, 59)"
+    
+    }
+
+
+
 
     function handleSubmitWord() {
         const currentWordArr = getCurrentWordArr()
         if(currentWordArr.length !== 5){
             window.alert("Your word must be 5 letters")
         }
-
         const currentWord = currentWordArr.join('')
+
+        const firstLetterId = guessedWordCount * 5 + 1;
+        const interval = 200
+        currentWordArr.forEach((letter, index) => {
+            setTimeout(() =>{
+                const tileColor = getTileColor(letter, index);
+
+                const letterId = firstLetterId + index;
+                const letterEl = document.getElementById(letterId);
+                letterEl.classList.add("animate__flipInX")
+                letterEl.style = `background-color: ${tileColor}; border-color: ${tileColor};`;
+            }, interval * index)
+        })
+
+
+
+
+
+
+        guessedWordCount += 1;
 
         if (currentWord === word){
             window.alert("Congratulations!")
@@ -25,6 +69,8 @@ document.addEventListener("DOMContentLoaded", () =>{
         }
 
         guessedWords.push([])
+
+
     }
 
     function getCurrentWordArr() {
@@ -43,11 +89,24 @@ document.addEventListener("DOMContentLoaded", () =>{
         }
     }
 
+    function handleDeleteLetter() {
+        const currentWordArr = getCurrentWordArr();
+        const removedLetter = currentWordArr.pop();
+
+        guessedWords[guessedWords.length - 1] = currentWordArr
+
+        const  lastLetterEl = document.getElementById(String(availableSpace - 1))
+
+        lastLetterEl.textContent = ''
+        availableSpace = availableSpace - 1
+    }
+
 
     function createSquares() {
         const gameBoard = document.getElementById("board")
         for (let index = 0; index < 30; index++){
             let square = document.createElement("div");
+            square.classList.add("animate__animated");
             square.classList.add("square");
             square.setAttribute("id", index + 1);
             gameBoard.appendChild(square)
@@ -59,6 +118,11 @@ document.addEventListener("DOMContentLoaded", () =>{
             const letter = target.getAttribute("data-key")
             if (letter === 'enter'){
                 handleSubmitWord()
+                return;
+            }
+
+            if (letter === 'del') {
+                handleDeleteLetter()
                 return;
             }
             updateGuessedWords(letter)
