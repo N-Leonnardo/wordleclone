4f93e1b480b4f9c4b52985c78476adbb6f9e82a4
Leonnardo Nascimento <leonnardoenriquesn@gmail.com>
2022-02-17 20:16:42 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    createSquares();
+
+    let guessedWords  = [[]]
+    let availableSpace = 1
+
+    let word = 'dairy'
+
+    const keys = document.querySelectorAll(".keyboard-row button");
+
+    function handleSubmitWord() {
+        const currentWordArr = getCurrentWordArr()
+        if(currentWordArr.length !== 5){
+            window.alert("Your word must be 5 letters")
+        }
+
+        const currentWord = currentWordArr.join('')
+
+        if (currentWord === word){
+            window.alert("Congratulations!")
+        }
+
+        if(guessedWords.length === 6) {
+            window.alert(`You lost ! The word was ${word}`)
+        }
+
+        guessedWords.push([])
+    }
+
+    function getCurrentWordArr() {
+        const numberOfGuessedWords = guessedWords.length;
+        return guessedWords[numberOfGuessedWords - 1]
+    }
+
+    function updateGuessedWords(letter){
+        const currentWordArr = getCurrentWordArr();
+        if (currentWordArr && currentWordArr.length < 5) {
+            currentWordArr.push(letter);
+            const availableSpaceEl = document.getElementById(String(availableSpace))
+            availableSpace += 1
+
+            availableSpaceEl.textContent = letter;
+        }
+    }
+
+
+    function createSquares() {
+        const gameBoard = document.getElementById("board")
+        for (let index = 0; index < 30; index++){
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.setAttribute("id", index + 1);
+            gameBoard.appendChild(square)
+        }
+    }
+
+    for (let i = 0; i< keys.length; i++){
+        keys[i].onclick = ({ target }) => {
+            const letter = target.getAttribute("data-key")
+            if (letter === 'enter'){
+                handleSubmitWord()
+                return;
+            }
+            updateGuessedWords(letter)
+        }
+    }
+
+
+})
